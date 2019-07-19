27cb07f81400757a11943d744faad1c3ba6eca61
Рико Инженер <48715538+danyanyam@users.noreply.github.com>
2019-07-19 09:36:48 +0300

    Add files via upload

---
+++ Parsers/Russian CB exchange rate/cb parser.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[41]:
+
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+
+
+def get_values(date_b, date_e, VAL_NM_RQ='R01235'):
+    urlis = 'http://www.cbr.ru/scripts/XML_dynamic.asp?'
+    html_doc = urlopen(urlis + 'date_req1=' + date_b + '&date_req2=' + date_e + '&VAL_NM_RQ=' + VAL_NM_RQ).read()
+    soup = BeautifulSoup(html_doc, features="html.parser")
+    data = soup.find_all('record')
+    records = {}
+    for record in data:
+        records.update({record['date']: record.text[1:]})
+    return records
+
+def clean_frame(records):
+    df = pd.DataFrame(data=records.items())
+    df.index = df[0]
+    df = df.drop([0], axis=1)
+    df.columns = ['USD']
+    return df
+
+def get_mean_rate(start, end):
+    df = clean_frame(get_values(start, end))
+    values = df.values
+    new = []
+    for n, i in enumerate(values):
+        new.append(float(i[0].replace(',', '.')))
+    array = np.array(new).mean()
+    return array
+
+get_mean_rate('01.01.2013', '01.01.2014')
+
+
+# In[ ]:
+
+
+
+
